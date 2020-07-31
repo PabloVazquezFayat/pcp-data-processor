5a86a24c59f3a2359dcfe680efb05a08f59cd228
Pablo Vazquez <pvazquez@vandldigital.com>
2020-07-31 14:43:07 -0400

    data processing working;

--- app.js
+++ app.js
@@ -24,12 +24,17 @@
 
 window.addEventListener('DOMContentLoaded', ()=>{
 
+    let loaderLabel = document.querySelector('#loader-label');
+    let loaderIcon = document.querySelector('#loader-icon');
+
     const parseExcel = function(e) {
 
         let file = e.target.files[0];
-        let json_data = [];
+        let json_data = {};
 
         if(!file){
+            loaderLabel.style.display = 'block';
+            loaderIcon.style.display = 'none';
             return;
         }
 
@@ -42,11 +47,14 @@ window.addEventListener('DOMContentLoaded', ()=>{
 
             for(let i = 0; i < workbook.SheetNames.length; i++){
                 var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[workbook.SheetNames[i]]);
-                var json_object = JSON.stringify(XL_row_object);
-                json_data.push(json_object);
+                json_data[workbook.SheetNames[i]] = XL_row_object;
             }
 
-            document.querySelector('#json-code-block').innerText = json_data.toString();
+            document.querySelector('#json-code-block').innerText = '';
+            document.querySelector('#json-code-block').innerText = JSON.stringify(json_data);
+
+            loaderLabel.style.display = 'block';
+            loaderIcon.style.display = 'none';
 
         };
 
@@ -56,8 +64,11 @@ window.addEventListener('DOMContentLoaded', ()=>{
     };
 
     let input = document.querySelector('#excel-file-input');
-    let button = document.querySelector('#excel-file-process');
 
-    input.addEventListener('change', parseExcel, false);
+    input.addEventListener('change', (e)=>{
+        loaderLabel.style.display = 'none';
+        loaderIcon.style.display = 'block';
+        parseExcel(e)
+    }, false);
 
 });
