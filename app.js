b7da865387c99bc457eb0ba987dfeea691aa2d3a
Pablo Vazquez <pvazquez@vandldigital.com>
2020-07-31 19:07:03 -0400

    search and remove duplicate objects in array

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-// all_columns_needed = [“Ship Code”,\
-//     “Sailing Date”,\
-//     “PRODUCT_ID”,\
-//     “Marketing Message”,\
-//     “Stamp”\
-//     ]
-// crf_columns_needed = [“Ship Code”,\
-//     “Sailing Date”,\
-//     “PRODUCT_ID”,\
-//     “Marketing Message”\
-//     ]
-// stamp_columns_needed = [“Ship Code”,\
-//       “Sailing Date”,\
-//       “PRODUCT_ID”,\
-//       “Stamp”\
-//      ]
-
 // STAMPS
 // [Array with Stamp dates];
 // Object with {“PRODUCT_ID”: {“dates_index”: {“ship_code”: “stamp_value”}}};
@@ -27,10 +10,10 @@ window.addEventListener('DOMContentLoaded', ()=>{
     let loaderLabel = document.querySelector('#loader-label');
     let loaderIcon = document.querySelector('#loader-icon');
 
-    const parseExcel = function(e) {
+    const parseExcel = function(e){
 
         let file = e.target.files[0];
-        let json_data = {};
+        let formatedData = {};
 
         if(!file){
             loaderLabel.style.display = 'block';
@@ -42,16 +25,68 @@ window.addEventListener('DOMContentLoaded', ()=>{
     
         reader.onload = (e)=> {
 
-            var data = e.target.result;
-            var workbook = XLSX.read(data, {type: 'binary'});
+            let data = e.target.result;
+            let workbook = XLSX.read(data, {type: 'binary'});
 
             for(let i = 0; i < workbook.SheetNames.length; i++){
-                var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[workbook.SheetNames[i]]);
-                json_data[workbook.SheetNames[i]] = XL_row_object;
+                let rowDataObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[workbook.SheetNames[i]]);
+
+                let valueData = rowDataObject.map((data)=>{
+                    return `${data['Ship Code']} ${data['Sailing Date']} ${data['PRODUCT_ID']} ${data['Marketing Message ']} ${data['Stamp']}`
+                });
+
+                let indices = [];
+
+                valueData.some((value, i)=>{
+                    if(valueData.indexOf(value) !== -1){
+                        indices.push(i);
+                    }
+                })
+
+                indices.forEach((indx, i)=>{
+                    console.log(`index: ${i}`, rowDataObject[indx]);
+                })
+
+                // console.log(isDuplicate);
+
+                let mappedData = rowDataObject.map((obj)=>{
+
+                    let dataObject = {};
+
+                    if(Object.keys(obj).indexOf('Ship Code') !== -1){
+                        dataObject['Ship Code'] = obj['Ship Code']
+                    }
+                    
+                    if(Object.keys(obj).indexOf('Sailing Date') !== -1){
+                        dataObject['Sailing Date'] = obj['Sailing Date']
+                    }
+                    
+                    if(Object.keys(obj).indexOf('PRODUCT_ID') !== -1){
+                        dataObject['PRODUCT_ID'] = obj["PRODUCT_ID"]
+                    }
+                    
+                    if( Object.keys(obj).indexOf('Marketing Message') !== -1   || 
+                        Object.keys(obj).indexOf('Marketing Message ') !== -1  ||
+                        Object.keys(obj).indexOf(' Marketing Message ') !== -1 ||
+                        Object.keys(obj).indexOf(' Marketing Message') !== -1
+                    ){
+                        dataObject['Marketing Message'] = obj['Marketing Message']
+                    }
+                    
+                    if(Object.keys(obj).indexOf('Stamp') !== -1){
+                        dataObject['Stamp'] = obj['Stamp']
+                    }
+
+                    return dataObject;
+                });
+
+                formatedData[workbook.SheetNames[i]] = mappedData;
             }
 
+            // console.log(formatedData);
+
             document.querySelector('#json-code-block').innerText = '';
-            document.querySelector('#json-code-block').innerText = JSON.stringify(json_data);
+            // document.querySelector('#json-code-block').innerText = JSON.stringify(formatedData);
 
             loaderLabel.style.display = 'block';
             loaderIcon.style.display = 'none';
@@ -59,7 +94,6 @@ window.addEventListener('DOMContentLoaded', ()=>{
         };
 
         reader.readAsBinaryString(file);
-
 
     };
 
@@ -70,5 +104,4 @@ window.addEventListener('DOMContentLoaded', ()=>{
         loaderIcon.style.display = 'block';
         parseExcel(e)
     }, false);
-
-});
+})
